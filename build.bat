c47b2853fe0da5e95a7f949982f817d16046d94d
Michael Georgoulopoulos <mgeorgoulopoulos@gmail.com>
2021-02-06 22:26:22 +0200

    Build script

--- build.bat
+++ build.bat
@@ -2,4 +2,13 @@ if not exist build mkdir build
 cd build
 cmake ..
 cmake --build .
+
+if exist osort del /F/Q/S osort\*
+if not exist osort mkdir osort
+copy Debug\osort.exe osort
+copy ..\TestList.txt osort
+copy ..\README.md osort
+if exist ..\osort.zip del ..\osort.zip
+tar.exe -a -c -f ..\osort.zip osort
+cd ..
 pause
